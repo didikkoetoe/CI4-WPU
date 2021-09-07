67669abbf82de3b8f82cd6289c14fb10196f301b
didikkoetoe <danieldidikprabowo@gmail.com>
2021-09-07 11:11:17 +0700

    Menambahkan method hapus dan edit

--- app/Controllers/Buku.php
+++ app/Controllers/Buku.php
@@ -71,4 +71,60 @@ class Buku extends BaseController
 		session()->setFlashdata('pesan', 'Data buku berhasil di tambahkan');
 		return redirect()->to('/buku');
 	}
+
+	public function hapus($id)
+	{
+		$this->bukuModel->delete($id);
+
+		session()->setFlashdata('pesan', 'Data buku berhasil di hapus');
+		return redirect()->to('/buku');
+	}
+
+	public function edit($slug)
+	{
+		$data = [
+			'title' => 'Edit Data',
+			'validation' => \Config\Services::validation(),
+			'buku' => $this->bukuModel->getBuku($slug)
+		];
+		return view('buku/edit', $data);
+	}
+
+	public function update($id)
+	{
+		// Cek apakah nama sama dengan yang di database untuk pengecekan
+		$bukuLama = $this->bukuModel->getBuku($this->request->getVar('slug'));
+		if($bukuLama['judul'] == $this->request->getVar('judul')) {
+			$rules = 'required';
+		} else {
+			$rules = 'required|is_unique[buku.judul]';
+		}
+
+		// Validasi
+		if (!$this->validate([
+			'judul' => [
+				'rules' => $rules,
+				'errors' => [
+					'required' => '{field} buku harus di isi',
+					'is_unique' => '{field} buku sudah ada.'
+				]
+			]
+		])) {
+			return redirect()->to('/Buku/tambah')->withInput();
+		}
+
+		$slug = url_title($this->request->getVar('judul'), '-', true);
+		$this->bukuModel->save([
+			'id' => $this->request->getVar('id'),
+			'judul' => $this->request->getVar('judul'),
+			'slug' => $slug,
+			'penulis' => $this->request->getVar('penulis'),
+			'penerbit' => $this->request->getVar('penerbit'),
+			'sampul' => $this->request->getVar('sampul')
+		]);
+
+		session()->setFlashdata('pesan', 'Data buku ' . $this->request->getVar('judul') . ' berhasil di ubah');
+
+		return redirect()->to('/buku');
+	}
 }
