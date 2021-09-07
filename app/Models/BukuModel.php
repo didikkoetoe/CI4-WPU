9ae6863599c5541bb48c0abc598ff5270d31b58d
didikkoetoe <danieldidikprabowo@gmail.com>
2021-09-07 07:14:31 +0700

    Model untuk buku

---
+++ app/Models/BukuModel.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class BukuModel extends Model
+{
+    protected $table      = 'buku';
+    protected $primaryKey = 'id';
+    protected $allowedFields = ['judul', 'slug', 'penulis', 'penerbit', 'sampul'];
+    protected $useTimestamps = true;
+
+    public function getBuku($slug = false)
+    {
+    	if($slug == false) {
+    		return $this->findAll();
+    	}
+
+    	return $this->where(['slug' => $slug])->first();
+    }
+}
