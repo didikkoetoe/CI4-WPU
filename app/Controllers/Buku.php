e34da5fdf6428b908890ad7951c34b9d2fdb12cb
didikkoetoe <danieldidikprabowo@gmail.com>
2021-09-07 19:22:38 +0700

    Menambahkan method upload

--- app/Controllers/Buku.php
+++ app/Controllers/Buku.php
@@ -55,17 +55,42 @@ class Buku extends BaseController
 					'required' => '{field} buku harus di isi',
 					'is_unique' => '{field} buku sudah ada.'
 				]
+			],
+			'sampul' => [
+				'rules' => 'max_size[sampul,1024]|is_image[sampul]|mime_in[sampul,image/jpg,image/jpeg,image/png]',
+				'errors' => [
+					'max_size' => 'Ukuran file terlalu besar, max 1mb',
+					'is_image' => 'Yang anda upload bukan gambar',
+					'mime_in' => 'Extensi file tidak di dukung'
+				]
 			]
 		])) {
 			return redirect()->to('/Buku/tambah')->withInput();
 		}
+
+		// Kelola file sampul
+		$fileSampul = $this->request->getFile('sampul');
+
+		// Cek apakah ada file yang di upload
+		if($fileSampul->getError() == 4) {
+			$namaSampul = 'default.png';
+		} else {
+		// nama sampul random
+		$namaSampul = $fileSampul->getRandomName();
+		// Pindahkan dir file ke path
+		$fileSampul->move('img', $namaSampul);
+		// nama file sama dengan nama file yang di upload
+		// $namaFile = $fileSampul->getName();
+		}
+
+
 		$slug = url_title($this->request->getVar('judul'), '-', true);
 		$this->bukuModel->save([
 			'judul' => $this->request->getVar('judul'),
 			'slug' => $slug,
 			'penulis' => $this->request->getVar('penulis'),
 			'penerbit' => $this->request->getVar('penerbit'),
-			'sampul' => $this->request->getVar('sampul')
+			'sampul' => $namaSampul
 		]);
 
 		session()->setFlashdata('pesan', 'Data buku berhasil di tambahkan');
