58d0a110f7d3dac48cb3146d96c1097ffb938cd9
didikkoetoe <danieldidikprabowo@gmail.com>
2021-10-12 11:31:43 +0700

    Menambahkan list filter baru dari MyAuth ko Config filter

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -19,6 +19,9 @@ class Filters extends BaseConfig
 		'csrf'     => CSRF::class,
 		'toolbar'  => DebugToolbar::class,
 		'honeypot' => Honeypot::class,
+		'login'      => \Myth\Auth\Filters\LoginFilter::class,
+    	'role'       => \Myth\Auth\Filters\RoleFilter::class,
+    	'permission' => \Myth\Auth\Filters\PermissionFilter::class,
 	];
 
 	/**
@@ -29,7 +32,9 @@ class Filters extends BaseConfig
 	 */
 	public $globals = [
 		'before' => [
-			// 'honeypot',
+			'honeypot',
+			//  * Jika di aktifkan akan memblock semua akses halaman yang kita buat, berlaku untuk semua controller global
+			// 'login',
 			// 'csrf',
 		],
 		'after'  => [
@@ -58,5 +63,9 @@ class Filters extends BaseConfig
 	 *
 	 * @var array
 	 */
-	public $filters = [];
+	public $filters = [
+		'login' => [
+			'before' => [
+				'buku']]
+	];
 }
